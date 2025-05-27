705907f0de0ad8d1db11eb10cfc6804ea6717fb2
Fran Alfaro <francisco.rodriguez.a@outsourcing.cepsa.com>
2025-05-27 18:15:59 +0200

    Terminar de cargar el JSON

--- francisco-rodriguez-alfaro/src/francisco_rodriguez_alfaro/clases/powerPlant.py
+++ francisco-rodriguez-alfaro/src/francisco_rodriguez_alfaro/clases/powerPlant.py
@@ -8,22 +8,34 @@ class FieldName():
     kerosine = "kerosine(euro/MWh)"
     co2 = "co2(euro/ton)"
     wind = "wind(%)"
+    
+    name = "name"
+    type = "type"
+    efficiency = "efficiency"
+    pmin = "pmin"
+    pmax = "pmax"
+    
 
 
 class Fuel():
     
     def __init__(self, data_object_fuels):
-        
-        
         self.gas = data_object_fuels.get(FieldName.gas)
         self.kerosine = data_object_fuels.get(FieldName.kerosine)
         self.co2 = data_object_fuels.get(FieldName.co2)
         self.wind = data_object_fuels.get(FieldName.wind)
+
+class PowerPlant():
+    
+    def __init__(self, data_object_powerplant):
         
-        
+        self.name = data_object_powerplant.get(FieldName.name)
+        self.type = data_object_powerplant.get(FieldName.type)
+        self.efficiency = data_object_powerplant.get(FieldName.efficiency)
+        self.pmin = data_object_powerplant.get(FieldName.pmin)
+        self.pmax = data_object_powerplant.get(FieldName.pmax)
         
         pass 
-        
         
 
 class ResumePowerPlant:
@@ -43,17 +55,18 @@ class ResumePowerPlant:
     def get_fuels(data_object_fuels):
         
         fuel = Fuel(data_object_fuels)
-        
         return fuel
         
     
     def get_powerplants(data_object_powerplants):
         
         powerplants = []
-        
         for plant in data_object_powerplants:
             
-            pass 
+            obj_plan = PowerPlant(plant)
+            powerplants.append(obj_plan)
+            
+        return powerplants
         
         
         
